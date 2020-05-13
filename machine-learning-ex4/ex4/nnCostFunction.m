bbaba7b1d88f49988ebabff98a7e68b9db3c5b89
Rushi Bhatt <rybhatt27@gmail.com>
2020-05-14 01:52:27 +0530

    may_14_update

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -63,31 +63,84 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
-bias = ones(m, 1);
+bias = ones(m, 1);          % 5000 x 1
 
-new_X = [bias X];
-output1 = new_X*Theta1';
-a1 = sigmoid(output1);
+X = [bias X];          % 5000 x 401
+z2 = X*Theta1';        % 5000 x 25
+a2 = sigmoid(z2);      % 5000 x 25
 
-activate1 = [bias a1];
-output2 = activate1*Theta2';
-a2 = sigmoid(output2);
+a2 = [bias a2];         % 5000 x 26
+z3 = a2*Theta2';        % 5000 x 10
+h = sigmoid(z3);       % 5000 x 10
 
-h = a2;
+[newopvalue new_a2] = max(a2, [], 2); % 5000 x 1
 
-for i=1:size(h,1)
-  for j=1:size(h,2)
-    J = J - (1/m)*( y(i)*log(h(i,j)) + (1-y(i))*log(1-h(i,j)));
-  endfor
+new_y = zeros(size(h,1), size(h,2));  % 5000 x 10
+
+for i=1:m
+  new_y(i, y(i)) = 1;
 endfor
 
 
+for i=1:size(h,1)
+  for j=1:size(h,2)
+    J = J - (1/m)*(new_y(i,j)*log(h(i,j)) + (1-new_y(i,j))*log(1-h(i,j)));
+  endfor
+endfor
 
+for i=1:size(Theta1,1)
+  for j=2:size(Theta1,2)
+    J = J + (lambda/(2*m))*(Theta1(i,j)^2);
+  endfor
+endfor
 
+for i=1:size(Theta2,1)
+  for j=2:size(Theta2,2)
+    J = J + (lambda/(2*m))*(Theta2(i,j)^2);
+  endfor
+endfor
 
+for i=1:m
+  
+  a1 = X(i,:);      % 1 x 401
+  a1 = a1';         % 401 x 1
+  z2 = Theta1*a1;   % (25 x 401) x (401 x 1)
+  a2 = sigmoid(z2); % 25 x 1
+  
+  a2 = [1; a2];     % 26 x 1
+  z3 = Theta2*a2;   % (10 x 26) x (26 x 1)
+  a3 = sigmoid(z3); % 10 x 1
+  
+  y3 = new_y(i,:);  % 1 x 10
+  y3 = y3';
+  
+  delta3 = a3 - y3; % 10 x 1
+  
+  z2 = [1; z2];     % 26 x 1
+  
+  delta2 = (Theta2' * delta3).*sigmoidGradient(z2); % (26 x 10) x (10 x 1)
+  
+  delta2 = delta2(2:end); % 25 x 1
+    
+  Theta2_grad = Theta2_grad + delta3*a2'; % (10 x 1) x (1 x 26)
+  Theta1_grad = Theta1_grad + delta2*a1'; % (25 x 1) x (1 x 401)
+  
+endfor
 
+Theta2_grad = (1/m)*Theta2_grad;
+Theta1_grad = (1/m)*Theta1_grad;
 
+for i=1:size(Theta1,1)
+  for j=2:size(Theta1,2)
+    Theta1_grad(i,j) = Theta1_grad(i,j) + (lambda/m)*Theta1(i,j);
+  endfor
+endfor
 
+for i=1:size(Theta2,1)
+  for j=2:size(Theta2,2)
+    Theta2_grad(i,j) = Theta2_grad(i,j) + (lambda/m)*Theta2(i,j);
+  endfor
+endfor
 
 
 % -------------------------------------------------------------
